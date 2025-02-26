fce933423a836b5578b84f3d7c443727322ce8bf
argg9822 <argg9822@gmail.com>
2025-02-26 15:35:07 -0500

    Initial commit with nextJs

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {  
+
+  return (
+    <>
+      <div className="bg-red-100">
+        <h1>Welcome to my portfolio</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
