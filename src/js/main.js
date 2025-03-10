d2d55f6eddafd5580ea0bd4c5105e9c9bcad221a
argg9822 <argg9822@gmail.com>
2025-03-09 22:16:13 -0500

    [Update] Add experience section. Improve projects section

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,20 @@ function initializeBasicsSlides (){
   splide.mount();
 }
 
+function rebaseTopMenu(){
+  console.log("Scrolling")
+  const mainContainer = document.getElementById('app');
+  const titles = document.getElementsByClassName('section-title');
+
+  Array.from(titles).forEach(title => {
+    console.log(title.getBoundingClientRect().top);
+    
+  });
+  console.log(mainContainer.getBoundingClientRect().top);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   initializeBasicsSlides();
 });
+
+document.addEventListener('scroll', rebaseTopMenu());
